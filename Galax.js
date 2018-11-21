0e18102e4c79196e8528f0318a9fc973c8038961
0zzymandias <30847435+0zzymandias@users.noreply.github.com>
2018-11-21 17:08:23 -0600

    Javascript tinkering

---
+++ Galax.js
@@ -0,0 +1,5 @@
+//Import libraries.
+
+console.log("Yo yo yo!")
+
+console.log("Normal %cStyled %clorem %cipsum", "color: blue; font-weight: bold", "color: red", "background-image: linear-gradient(red, blue); color: white; padding: 5px;")
