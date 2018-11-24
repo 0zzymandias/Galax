4b37310a0c667081c24411e68169eaa655f12e3d
0zzymandias <30847435+0zzymandias@users.noreply.github.com>
2018-11-24 12:40:47 -0600

    JS function learning

--- Galax.js
+++ Galax.js
@@ -3,3 +3,10 @@
 console.log("Yo yo yo!")
 
 console.log("Normal %cStyled %clorem %cipsum", "color: blue; font-weight: bold", "color: red", "background-image: linear-gradient(red, blue); color: white; padding: 5px;")
+
+function adder(x,y) {
+    z = x+y
+}
+
+adder(1,3)
+console.log(z)
