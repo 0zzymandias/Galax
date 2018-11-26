7eaa5435c0ebaa94729d302ef0b58885d633cf18
0zzymandias <30847435+0zzymandias@users.noreply.github.com>
2018-11-26 17:14:27 -0600

    Added calculation comment

--- combat.py
+++ combat.py
@@ -3,3 +3,5 @@
 def attack(x,y):
     attack1 = x*((100+y)/100)
     print(attack1)
+#Gross Damage Output = Base * ((100 + strength)/100)) || At 100 points of str, player should deal 2x their base damage every hit.
+#Attack1 will feed into Attack2 calculations and so forth, factoring dodge, etc.
